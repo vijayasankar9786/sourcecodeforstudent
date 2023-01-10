ec27150cd5430e050c73a8b4ce69bbd00cc34b96
Vijayasankar Murugan <85429796+vijayasankar9786@users.noreply.github.com>
2023-01-10 17:19:49 +0700

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,6 @@
+function subscribe() {
+    window.open( "https://www.youtube.com/channel/UCnShImWEdbCUkDa3uUEt-IA");
+    var dbutton = document.getElementById("download");
+    dbutton.style.display = "block";
+}
+
